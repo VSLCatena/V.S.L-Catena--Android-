73b898c29d2c08bf1eff19a7d8761ac1f8b9e9a8
Sean <seandijk@gmail.com>
2019-04-18 00:17:11 +0200

    Added behaviour to the usersettings fragment. User can upload profile pic when set up in firebase rules

--- app/src/main/java/nl/vslcatena/vslcatena/modules/settings/UserSettingsFragment.kt
+++ app/src/main/java/nl/vslcatena/vslcatena/modules/settings/UserSettingsFragment.kt
@@ -1,8 +1,18 @@
 package nl.vslcatena.vslcatena.modules.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_settings.*
 import nl.vslcatena.vslcatena.R
+import nl.vslcatena.vslcatena.util.Paths
+import nl.vslcatena.vslcatena.util.extensions.setImageFromFirebaseStorage
+import nl.vslcatena.vslcatena.util.login.UserProvider
+import nl.vslcatena.vslcatena.util.selectingFiles.letUserPickImage
+import nl.vslcatena.vslcatena.util.selectingFiles.onUserPickedImage
 
 /**
  * Fragment where the user can adjust setting relating to his account:
@@ -13,5 +23,39 @@ import nl.vslcatena.vslcatena.R
 class UserSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        UserProvider.getUser()?.let {
+            username.text = it.name
+            userId.text = "LIT-nummer?"
+            userId.text = it.email
+            profileImage.setImageFromFirebaseStorage(Paths.getStorageThumbnailPath(it.id))
+
+            profileImage.setOnClickListener {
+                letUserPickImage()
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.let {
+            onUserPickedImage(requestCode, resultCode, it) { imageUri ->
+                profileImage.setImageURI(imageUri)
+
+                //todo should upload to somewhere else than thumbnail and then create on using a firebase function
+                FirebaseStorage.getInstance()
+                        .getReference(Paths.getStorageThumbnailPath(UserProvider.getUser()!!.id))
+                        .putFile(imageUri!!)
+                        .addOnSuccessListener { Toast.makeText(this.context, "profile picture uploaded succesfully", Toast.LENGTH_LONG).show() }
+                        .addOnFailureListener { Toast.makeText(this.context, "profile picture upload failed", Toast.LENGTH_LONG).show()}
+
+            }
+        }
+
     }
 }
